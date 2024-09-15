e98bd54a1415aeca9122877e4150ac050f94d770
Colejacob26 <jacob.cole@csuglobal.edu>
2024-09-15 14:11:14 -0400

    Add files via upload

--- CriticalThinking#1.py
+++ CriticalThinking#1.py
@@ -1,15 +1,23 @@
 print("Please enter two numbers")
+#Requests two numbers
 num1 = int(input())
 num2 = int(input())
+#collects numbers input
 
+
+#Shows statments adding and subtracting numbers collected
 print(str(num1), '+', str(num2), '=', (num1 + num2))
 print(str(num1), '-', str(num2), '=', (num1 - num2))
 
-
+#Part 2 started
+#Requests two Numbers
 print("Please enter two numbers")
 num1 = int(input())
 num2 = int(input())
+#collects numbers input
 
+
+#Shows statments multiplying and dividing numbers collected
 print(str(num1), '*', str(num2), '=', (num1 * num2))
 print(str(num1), '%', str(num2), '=', (num1 / num2))
 
