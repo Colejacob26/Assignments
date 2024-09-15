f4f73fc507a5c0ca7b75d1163c100b833530eb26
Colejacob26 <jacob.cole@csuglobal.edu>
2024-09-15 13:53:34 -0400

    Add files via upload

---
+++ CriticalThinking#1.py
@@ -0,0 +1,15 @@
+print("Please enter two numbers")
+num1 = int(input())
+num2 = int(input())
+
+print(str(num1), '+', str(num2), '=', (num1 + num2))
+print(str(num1), '-', str(num2), '=', (num1 - num2))
+
+
+print("Please enter two numbers")
+num1 = int(input())
+num2 = int(input())
+
+print(str(num1), '*', str(num2), '=', (num1 * num2))
+print(str(num1), '%', str(num2), '=', (num1 / num2))
+
